bd4d0d2ac4709f50d7c86e1ea996f33dbe1e66ac
Simon Rose <simonrose121@gmail.com>
2015-02-10 11:52:59 +0000

    Begun implementing DAO

--- DAO.js
+++ DAO.js
@@ -1,5 +1,14 @@
+var mongoose = require('mongoose');
+
+var db = mongoose.connection;
+
+// setup settings
 var DAO = function() {
-    
+
 }
+
+// define schema
+
+// methods to get data
 
 exports.module = DAO;
