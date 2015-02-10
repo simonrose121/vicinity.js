39440770db19b562966fddecb42126eb6675a96f
Simon Rose <simonrose121@gmail.com>
2015-02-10 11:07:56 +0000

    Node distance function added and passing tests

--- model/node.js
+++ model/node.js
@@ -5,10 +5,26 @@ function node(lat, lon, alt) {
     this._alt = alt;
 }
 
-// class methods
-
-// getters
-
-// setters
+node.prototype.distance = function(otherNode) {
+    var R = 6371000; // earths radius in metres
+    var φ1 = this._lat.toRad();
+    var φ2 = otherNode._lat.toRad();
+    var Δφ = (otherNode._lat-this._lat).toRad();
+    var Δλ = (otherNode._lon-this._lon).toRad();
+    
+    var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+            Math.cos(φ1) * Math.cos(φ2) *
+            Math.sin(Δλ/2) * Math.sin(Δλ/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    
+    var d = R * c;
+    return Math.round(d);
+}
 
 module.exports = node;
+
+if (typeof(Number.prototype.toRad) === "undefined") {
+  Number.prototype.toRad = function() {
+    return this * Math.PI / 180;
+  }
+}
--- tests/node.spec.js
+++ tests/node.spec.js
@@ -10,3 +10,13 @@ describe("Constructing a node object", function() {
         });
     });
 });
+
+describe("Calculating distance between two nodes", function() {
+    it("Distance calculation", function() {
+        runs(function(){
+            var myNode = new node(53.378491, -1.472634, 4.5);
+            var otherNode = new node(53.379720, -1.445426, 0);
+            expect(myNode.distance(otherNode)).toEqual(1810);
+        });
+    });
+});
